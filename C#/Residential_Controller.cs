e8c218d90923357f2d2d8557713fefb5fae73b75
IsaacLaquerre <60407229+IsaacLaquerre@users.noreply.github.com>
2020-02-24 12:16:28 -0500

    Update Residential_Controller.js & C# initial commit

--- C#/Residential_Controller.cs
+++ C#/Residential_Controller.cs
@@ -1,33 +1,35 @@
-class Column {
-    constructor(floors, elevators) {
+public class Column {
+    public int floors;
+    public int elevators;
+    public List<object> elevatorsList;
+    public List<object> externalButtonList;
+    public Column(int floors, int elevators){
         this.floors = floors;
         this.elevators = elevators;
-        this.externalButtonList = [];
-        this.elevatorsList = [];
-        for (let i = 0; i < elevators; i++) {
-            this.elevatorsList.push(new Elevator(0, floors));
+        for (int i = 0; i < elevators; i++) {
+            this.elevatorsList.Add(new Elevator(0, floors));
         }
-		for (let i = 0; i < this.floors; i++) {
-            if (i === 0) {
-                this.externalButtonList.push(new ExternalButton(i, "up", false));
+		for (int i = 0; i < this.floors; i++) {
+            if (i == 0) {
+                this.externalButtonList.Add(new ExternalButton(i, "up", false));
             }else {
-                this.externalButtonList.push(new ExternalButton(i, "up", false));
-                this.externalButtonList.push(new ExternalButton(i, "down", false));
+                this.externalButtonList.Add(new ExternalButton(i, "up", false));
+                this.externalButtonList.Add(new ExternalButton(i, "down", false));
             }
 		}
     }
 
-    findElevator(requestedFloor, direction) {
+    function findElevator(int requestedFloor, string direction) {
 
-		var chosenElevator = null;
-		var bestGap = this.floors;
+        int chosenElevator = null;
+        int bestGap = this.floors;
 
 		for (let i = 0; i < this.elevatorsList.length; i++) {
-			if (this.elevatorsList[i].direction === "up" && direction === "up" && requestedFloor > this.elevatorsList[i].currentFloor) {
+			if (this.elevatorsList[i].direction == "up" && direction == "up" && requestedFloor > this.elevatorsList[i].currentFloor) {
                 chosenElevator = this.elevatorsList[i];
-			}else if (this.elevatorsList[i].direction === "down" && direction === "down" && requestedFloor < this.elevatorsList[i].currentFloor) {
+			}else if (this.elevatorsList[i].direction == "down" && direction == "down" && requestedFloor < this.elevatorsList[i].currentFloor) {
                 chosenElevator = this.elevatorsList[i];
-			}else if (this.elevatorsList[i].status === "idle") {
+			}else if (this.elevatorsList[i].status == "idle") {
 				chosenElevator = this.elevatorsList[i];
 			}else {
 				for (let i = 0; i < this.elevatorsList.length; i++) {
@@ -39,14 +41,13 @@ class Column {
                 }
 			}
 		}
-		console.log("Best elevator found on floor " + chosenElevator.currentFloor);
+		console.write("Best elevator found on floor " + chosenElevator.currentFloor);
 		return chosenElevator;
-	}
-
-	requestElevator(requestedFloor, direction) {
+    }
+	requestElevator(int requestedFloor, string direction) {
 		if (requestedFloor > this.floors) return console.log("Floor " + requestedFloor + " doesn't exist!");
 
-		console.log("Called an elevator to the floor " + requestedFloor);
+		console.write("Called an elevator to the floor " + requestedFloor);
 
 		let elevator = this.findElevator(requestedFloor, direction);
 
@@ -55,37 +56,37 @@ class Column {
 		return elevator;
 	}
 
-	requestFloor(elevator, requestedFloor) {
-        console.log("Moving elevator on floor " + elevator.currentFloor + " to the floor " + requestedFloor);
+	requestFloor(object elevator, int requestedFloor) {
+        console.write("Moving elevator on floor " + elevator.currentFloor + " to the floor " + requestedFloor);
 		elevator.addToQueue(requestedFloor);
 		elevator.closeDoors();
 		elevator.move();
     }
 }
-
 class Elevator {
-	constructor(currentFloor, floors) {
+
+    public List<object> internalButtonsList;
+    public List<object> queue;
+	constructor(int currentFloor, int floors) {
 
 		this.direction = null;
 		this.floors = floors;
 		this.currentFloor = currentFloor;
 		this.status = "idle";
-		this.queue = [];
-		this.internalButtonsList = [];
 		this.door = "closed";
 
 		for (let i = 0; i < this.floors; i++) {
             this.internalButtonsList.push(new InternalButton(i));
         }
     }
-	addToQueue(requestedFloor) {
-		this.queue.push(requestedFloor)
+	addToQueue(int requestedFloor) {
+		this.queue.push(requestedFloor);
 
 		if (this.direction == "up") {
-			this.queue.sort((a, b) => a - b)
+			this.queue.sort((a, b) => a - b);
 		}
 		if (this.direction == "down") {
-			this.queue.sort((a, b) => b - a)
+			this.queue.sort((a, b) => b - a);
 		}
 
 		console.log("Added floor " + requestedFloor + " to the elevator's queue. Current queue: " + this.queue.join(", "));
@@ -96,11 +97,11 @@ class Elevator {
 
             let firstElement = this.queue[0];
 
-			if (this.door === "open") {
+			if (this.door == "open") {
 				console.log("Waiting 7 seconds for the doorway to be cleared");
 				this.closeDoors();
             }
-			if (firstElement === this.currentFloor) {
+			if (firstElement == this.currentFloor) {
 				this.queue.shift();
 				this.openDoors();
 			}
@@ -112,7 +113,7 @@ class Elevator {
 			if (firstElement < this.currentFloor) {
 				this.status = "moving";
 				this.direction = "down";
-				this.moveDown()
+				this.moveDown();
 			}
 		}
 		console.log("Waiting 7 seconds for the doorway to be cleared");
@@ -131,56 +132,67 @@ class Elevator {
 	}
 
 	openDoors() {
-			this.door = "open"
+			this.door = "open";
 			console.log("<> Opened doors");
 	}
 
 	closeDoors() {
-			this.door="closed"
+			this.door="closed";
 			console.log(">< Closed doors");
 	}
 
 }
 
 class ExternalButton {
-	constructor(requestFloor, direction) {
+	constructor(int requestFloor, string direction) {
 		this.requestFloor = requestFloor;
 		this.direction = direction;
 	}
 }
 
 class InternalButton {
-	constructor(floor) {
+	constructor(int floor) {
 		this.floor = floor;
 	}
 }
 
-
 /* -------------------------- TESTING SECTION -------------------------- */
 
+int Main() {
 
-//console.log("--------------------------------------- TEST #1 ------------------------------------------------------\n\n")
+
+console.write("--------------------------------------- TEST #1 ------------------------------------------------------\n\n");
 
 function Test1_requestElevator() {
 
+    List<int> queue1 = new List<int>();
+    int[] array1 = new int[] { 4, 5, 7 };
+    queue1.AddRange(array);
+    List<int> queue2 = new List<int>();
+    int[] array2 = new int[] { 4, 3 };
+    queue2.AddRange(array);
+    List<int> queue3 = new List<int>();
+    int[] array3 = new int[] { 1, 2, 5, 7 };
+    queue3.AddRange(array);
+
 	column1 = new Column(10, 2);
 
-	column1.elevatorsList[0].currentFloor = 2
-    	column1.elevatorsList[0].direction = "up"
-    	column1.elevatorsList[0].status = "moving"
-	column1.elevatorsList[0].queue = [4, 6, 7]
+	column1.elevatorsList[0].currentFloor = 2;
+    column1.elevatorsList[0].direction = "up";
+    column1.elevatorsList[0].status = "moving";
+	column1.elevatorsList[0].queue = queue1;
 
-	column1.elevatorsList[1].currentFloor = 6
-	column1.elevatorsList[1].direction = "down"
-	column1.elevatorsList[1].status = "moving"
-	column1.elevatorsList[1].queue = [4, 3]
+	column1.elevatorsList[1].currentFloor = 6;
+	column1.elevatorsList[1].direction = "down";
+	column1.elevatorsList[1].status = "moving";
+	column1.elevatorsList[1].queue = queue2;
 
 	column1.requestElevator(1, "down");
 }
 
 Test1_requestElevator();
 
-//console.log("\n\n--------------------------------------- TEST #2 ------------------------------------------------------\n\n")
+console.write("\n\n--------------------------------------- TEST #2 ------------------------------------------------------\n\n");
 
 function  Test2_requestFloor(){
 	column2 = new Column(10, 2);
@@ -188,11 +200,12 @@ function  Test2_requestFloor(){
 	column2.elevatorsList[0].currentFloor = 2;
 	column2.elevatorsList[0].direction  =  "down";
 	column2.elevatorsList[0].status =  "moving";
-	column2.elevatorsList[0].queue = [3,10,5,7,1];
+	column2.elevatorsList[0].queue = queue3;
 
 	elevator = column2.elevatorsList[0];
 
 	column2.requestFloor(elevator, 9);
 }
 
-//Test2_requestFloor();
+Test2_requestFloor();
+}
